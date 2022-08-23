adba3deb18309bd503d3eafa3bc764d33ff0f746
shintarou37 <m.sintarou37@gmail.com>
2022-08-23 10:57:39 +0900

    [Launch] 一覧画面APIを作成

--- main.go
+++ main.go
@@ -4,29 +4,40 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	// "reflect"
+	"strconv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"reflect"
 )
+
 const StatusInternalServerError int = 500
 
 type Mst_situation struct {
-    gorm.Model
-    Name    string
-    Musics  []Music
-  }
-  
-type Music struct {
-    gorm.Model
-    Name    string
-    Artist  string
-    Reason  string
-    Mst_situationID uint
+	gorm.Model
+	Name   string
+	Musics []Music
 }
 
-type Response1 struct {
-    Mst_situation   string
-    Music   string
+type Music struct {
+	gorm.Model
+	Name            string
+	Artist          string
+	Reason          string
+	Mst_situationID int
+}
+
+type ResponseTop struct {
+	Mst_situation string
+	Music         string
+}
+
+type MusicResult struct {
+	gorm.Model
+	Name            	string
+	Artist          	string
+	Reason          	string
+	Mst_situationID		int
+	Mst_situationName   string
 }
 
 // グローバルスコープとして定義することで、本ファイルのどの関数でも引数の受け渡しなしに使用可能にする。
@@ -34,139 +45,147 @@ var db *gorm.DB
 var db_err error
 
 func main() {
-    fmt.Println("Start!");
-    dsn := "root:@tcp(127.0.0.1:3306)/music?charset=utf8mb4&parseTime=True&loc=Local"
-    db, db_err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if db_err != nil {
+	fmt.Println("Start!")
+	dsn := "root:@tcp(127.0.0.1:3306)/music?charset=utf8mb4&parseTime=True&loc=Local"
+	db, db_err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if db_err != nil {
 		panic(db_err)
 	}
 
-    http.HandleFunc("/", top);
-    http.HandleFunc("/detail", detail);
-    http.HandleFunc("/register", register);
-    http.ListenAndServe(":8080", nil)
-}
-/* 
-    Top画面 
-*/
-func top(w http.ResponseWriter, r *http.Request){
-    fmt.Println("パス（\"/\"）でGOが呼び出された")
-
-    // ヘッダーをセットする（エラー処理後にセットするとCROSエラーになる）
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Headers", "*")
-    w.Header().Set("Content-Type", "application/json")
-
-    // 全レコードを取得する
-    music, situation, orm_err := ReadMulti()
-    fmt.Println(reflect.TypeOf(music))
-    fmt.Println(reflect.TypeOf(situation))
-    // jsonエンコード
-    situationJson, err := json.Marshal(situation)
-    musicJson, err := json.Marshal(music)
-    // fmt.Println(reflect.TypeOf(situationJson))
-    // fmt.Println(reflect.TypeOf(musicJson))
-    // arr := [2]string{string(situationJson), string(musicJson)}
-    // outputJson, err := json.Marshal(arr)
-    var res = Response1{Mst_situation: string(situationJson), Music: string(musicJson)}
-    outputJson, err := json.Marshal(res)
-    // エラー処理
-    if err != nil || !orm_err{
-        fmt.Println("error happen!")
-        w.WriteHeader(http.StatusInternalServerError)
-    }
-
-    // jsonデータを返却する（エラーが発生した場合は空のオブジェクトを返却する）
-    fmt.Fprint(w, string(outputJson))
+	http.HandleFunc("/", top)
+	http.HandleFunc("/detail", detail)
+	http.HandleFunc("/register", register)
+	http.ListenAndServe(":8080", nil)
 }
 
-/* 
-    詳細画面
+/*
+   Top画面
 */
-func detail(w http.ResponseWriter, r *http.Request){
-    fmt.Println("パス（\"/detail\"）でGOが呼び出された")
+func top(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("パス（\"/\"）でGOが呼び出された")
 
-    // ヘッダーをセットする
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Content-Type", "application/json")
+	// ヘッダーをセットする（エラー処理後にセットするとCROSエラーになる）
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Content-Type", "application/json")
 
-    // クエリパラメータ「id」を取得する
-    var id string = r.URL.Query().Get("id")
+	// 全レコードを取得する
+	music, situation, orm_err := ReadMulti()
+	// fmt.Println(reflect.TypeOf(music))
+	// fmt.Println(reflect.TypeOf(situation))
+	// jsonエンコード
+	situationJson, err := json.Marshal(situation)
+	musicJson, err := json.Marshal(music)
+	// fmt.Println(reflect.TypeOf(situationJson))
+	// fmt.Println(reflect.TypeOf(musicJson))
+	// arr := [2]string{string(situationJson), string(musicJson)}
+	// outputJson, err := json.Marshal(arr)
+	var res = ResponseTop{Mst_situation: string(situationJson), Music: string(musicJson)}
+	outputJson, err := json.Marshal(res)
+	// エラー処理
+	if err != nil || !orm_err {
+		fmt.Println("error happen!")
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	fmt.Fprint(w, string(outputJson))
+}
 
-    // React側で画面をリロードするとクエリパラメータがundefinedで送付される
-    // その場合は"false"という文字列がパラメーターとして送信されてsqlは発行しない
+/*
+   詳細画面
+*/
+func detail(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("パス（\"/detail\"）でGOが呼び出された")
+
+	// ヘッダーをセットする
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	// クエリパラメータ「id」を取得する
+	var id string = r.URL.Query().Get("id")
+
+	// React側で画面をリロードするとクエリパラメータがundefinedで送付される
+	// その場合は"false"という文字列がパラメーターとして送信されてsqlは発行しない
 	if id == "false" {
 		panic("no query params")
-        // これ以降の処理は行われない
+		// これ以降の処理は行われない
 	}
-    
-    ret, orm_err := Read(id)
 
-    // jsonエンコード
-    outputJson, err := json.Marshal(ret)
+	ret, orm_err := Read(id)
 
-    // エラー処理
-    if err != nil || !orm_err {
-        fmt.Println("error happen!")
-        w.WriteHeader(http.StatusInternalServerError)
-    }
+	// jsonエンコード
+	outputJson, err := json.Marshal(ret)
 
-    // jsonデータを返却する
-    fmt.Fprint(w, string(outputJson))
-}
-/* 
-    登録機能
-*/
-func register(w http.ResponseWriter, r *http.Request){
-    fmt.Println("パス（\"/register\"）でGOが呼び出された")
-    // 登録機能時にOPTIONSリクエストが送付される
-	fmt.Println(r.Method)
-	if r.Method != http.MethodPost {
-        return
-    }
-    // クエリパラメータに含まれた値を使用して構造体を初期化する。
-    var create = Music{Name: r.URL.Query().Get("name"), Reason: r.URL.Query().Get("reason"), Artist: r.URL.Query().Get("artist")}
-
-    // レコードの作成
-    if orm_err := db.Debug().Create(&create).Error; orm_err != nil {
-        fmt.Println("error happen!")
+	// エラー処理
+	if err != nil || !orm_err {
+		fmt.Println("error happen!")
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 
-    // // ヘッダーをセットする
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Headers", "*")
-
-    // データを返却する
-    fmt.Fprint(w, true)
-}
-/* 
-    パス：top
-*/
-func ReadMulti()([]Music, []Mst_situation, bool){
-    var music_arr []Music
-    var situation_arr []Mst_situation
-    // return music_arr, false
-    if err := db.Debug().Find(&music_arr).Error; err != nil {
-		return music_arr, situation_arr, false
-	}
-
-    if err := db.Debug().Find(&situation_arr).Error; err != nil {
-		return music_arr, situation_arr, false
-	}
-    return music_arr, situation_arr, true
+	// jsonデータを返却する
+	fmt.Fprint(w, string(outputJson))
 }
 
-/* 
-    パス：detail
+/*
+   登録機能
 */
-func Read(id string) (Music, bool){
-    var music Music
-    // return music, false
-    // ポインタを引数にしない場合はエラーになる
-    if err := db.Debug().First(&music, id).Error; err != nil {
-        fmt.Println("error happen!")
+func register(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("パス（\"/register\"）でGOが呼び出された")
+	// 登録機能時にOPTIONSリクエストが送付される
+	fmt.Println(r.Method)
+	if r.Method != http.MethodPost {
+		return
+	}
+
+	// Mst_situationIDをint型に変換する
+	var situationID int
+	var s string = r.URL.Query().Get("situation")
+	situationID, _ = strconv.Atoi(s)
+
+	// クエリパラメータに含まれた値を使用して構造体を初期化する。
+	var create = Music{Name: r.URL.Query().Get("name"), Reason: r.URL.Query().Get("reason"), Artist: r.URL.Query().Get("artist"), Mst_situationID: situationID}
+
+	// レコードの作成
+	if orm_err := db.Debug().Create(&create).Error; orm_err != nil {
+		fmt.Println("error happen!")
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// // ヘッダーをセットする
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	// データを返却する
+	fmt.Fprint(w, true)
+}
+
+/*
+   パス：top
+*/
+func ReadMulti() ([]MusicResult, []Mst_situation, bool) {
+	var music_result []MusicResult
+	var situation_arr []Mst_situation
+
+	if err := db.Table("musics").Debug().Select("musics.id, musics.name, musics.artist, musics.reason, musics.mst_situation_id, `mst_situations`.name AS Mst_situationName").Joins("INNER JOIN mst_situations AS `mst_situations` ON `musics`.mst_situation_id = `mst_situations`.id").Find(&music_result).Error; err != nil {
+	    fmt.Println(err)
+		return music_result, situation_arr, false
+	}
+
+	if err := db.Debug().Find(&situation_arr).Error; err != nil {
+		return music_result, situation_arr, false
+	}
+	return music_result, situation_arr, true
+}
+
+/*
+   パス：detail
+*/
+func Read(id string) (Music, bool) {
+	var music Music
+	// return music, false
+	// ポインタを引数にしない場合はエラーになる
+	if err := db.Debug().First(&music, id).Error; err != nil {
+		fmt.Println("error happen!")
 		return music, true
 	}
-    return music, true
+	return music, true
 }
